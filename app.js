725acd23ccc839dd9b23885cbbc4f51930ae8d58
Shubham Kumar Vishwakarma <shubhamv387@gmail.com>
2023-08-06 18:02:07 +0530

    fixed undefined issue completly

--- app.js
+++ app.js
@@ -8,8 +8,16 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/login", (req, res, next) => {
   res.send(
-    `<form onsubmit="localStorage.setItem('username', document.getElementById('username').value)" action="/" method="POST"><input type="text" id="username" name="username" /><br><input type= "submit" value='LOG IN' /><form>`
+    `<form  action="/login" method="POST" onsubmit="localStorage.setItem('username', document.getElementById('username').value)">
+        <input type="text" id="username" name="username" />
+        <br>
+        <input type= "submit" value='LOG IN' />
+    <form>`
   );
+});
+
+app.post("/login", (req, res, next) => {
+  res.redirect("/");
 });
 
 app.get("/", (req, res, next) => {
@@ -18,29 +26,33 @@ app.get("/", (req, res, next) => {
   fs.readFile("message.txt", "utf-8", (err, data) => {
     if (err) {
       console.log(err);
+      data = "";
     }
     res.send(
-      `${data}<br><form action="/" onsubmit="document.getElementById('username').value=localStorage.getItem('username')" method = "POST"><input type="hidden" name="username" id="username"><input type="text" name="message" /><br><input type= "submit" value='SEND' /><form>`
+      `${data}<form action="/" method = "POST" onsubmit="document.getElementById('username').value=localStorage.getItem('username')" >
+        <input type="hidden" name="username" id="username">
+        <input type="text" name="message" />
+        <br>
+        <input type= "submit" value='SEND' />
+      <form>`
     );
   });
 });
 
 app.post("/", (req, res, next) => {
   //   console.log(req.body.username, req.body.message);
-  fs.appendFile(
+  fs.writeFile(
     "message.txt",
-    `${req.body.username}: ${
-      req.body.message == undefined ? "Hello" : req.body.message
-    } `,
+    `${req.body.username}: ${req.body.message}, `,
+    { flag: "a" },
     (err) => {
-      if (err) console.log(err);
+      err ? console.log(err) : res.redirect("/");
     }
   );
-  res.redirect("/");
 });
 
 app.use((req, res, next) => {
-  res.send("<h1>Page not Found</h1>");
+  res.status(404).send("<h1>Page not Found</h1>");
 });
 
 app.listen(3000);
